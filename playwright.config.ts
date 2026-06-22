5ec3eb4cd7176fac1121c3d151dc1fd0dd135e06
Josiah Sayers <47199245+JosiahSayers@users.noreply.github.com>
2026-06-21 20:07:43 -0400

    setup playwright testing (#9)
    * setup playwright testing
    
    * delete playwright test-results directory
    
    * ignore playwright test-results directory
    
    * use health endpoint to test if app is ready for tests to begin
    
    * fix bun trying to run playwright tests and try using /ready endpoint
    
    * run db migrations before checking for app to be up
    
    * remove wait step in playwright action
    
    * start application after setting up database
    
    * try to get artifacts to upload
    
    * setup better auth with the correct url

---
+++ playwright.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests/e2e",
+  testMatch: "**/*.e2e.ts",
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1,
+  reporter: process.env.CI ? [["github"], ["html", { open: "never" }]] : "html",
+  use: {
+    baseURL: process.env.BASE_URL ?? "http://localhost:3000",
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
